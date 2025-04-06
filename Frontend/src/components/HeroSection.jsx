79a8fcfa4a8fe861377d1a45148ee073ae3acae3
prasadt45 <146625931+prasadt45@users.noreply.github.com>
2025-04-06 17:58:55 +0530

    Update HeroSection image rotation and adjust interval timing for better user experience

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -8,10 +8,10 @@ import { useNavigate } from "react-router-dom";
 export default function HeroSection() {
   const images = [
     "https://firebasestorage.googleapis.com/v0/b/imentiv-assets/o/image-tool%2Fimage-emotion-2.png?alt=media&token=0895b7c1-4f2f-4bb2-8fe2-2417df199a7f",
-    "https://firebasestorage.googleapis.com/v0/b/imentiv-assets/o/image-tool%2Fimage-emotion-5.png?alt=media&token=6a763c75-f41c-4252-958d-5c56f2e1ecfc",
     "https://firebasestorage.googleapis.com/v0/b/imentiv-assets/o/image-tool%2Fimage-emotion-4.png?alt=media&token=bdbe6a49-884a-48c1-95ed-ce34c297f128",
-    "https://firebasestorage.googleapis.com/v0/b/imentiv-assets/o/image-tool%2Fimage-emotion-3.png?alt=media&token=1caff628-0884-42f8-a992-1332bce0c4b9",
     "https://firebasestorage.googleapis.com/v0/b/imentiv-assets/o/image-tool%2Fimage-emotion-1.png?alt=media&token=7204d450-68ae-48ef-aba6-cc2f940947ef",
+    "https://firebasestorage.googleapis.com/v0/b/imentiv-assets/o/image-tool%2Fimage-emotion-5.png?alt=media&token=6a763c75-f41c-4252-958d-5c56f2e1ecfc",
+    "https://firebasestorage.googleapis.com/v0/b/imentiv-assets/o/image-tool%2Fimage-emotion-3.png?alt=media&token=1caff628-0884-42f8-a992-1332bce0c4b9",
   ];
 
   const [currentImage, setCurrentImage] = useState(0);
@@ -26,7 +26,7 @@ export default function HeroSection() {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImage((prev) => (prev + 1) % images.length);
-    }, 800); // Change image every 800ms
+    }, 900); // Change image every 800ms
 
     return () => clearInterval(interval);
   }, []);
